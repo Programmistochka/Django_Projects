ae2d9c176acf109fb9e299dd27976eaaac04eed6
Programmistochka <programmistochka@list.ru>
2023-05-29 10:17:03 +1200

    Task_3 add commands to import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from phones.models import Phone
 
 
@@ -14,4 +15,14 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            print(phone)
+            print("==========================")
+            # print(f'{phone["name"]}: {slugify(phone["name"])}')
+            ph = Phone(name=phone['name'],
+                       price=phone['price'],
+                       image=phone['image'],
+                       release_date=phone['release_date'],
+                       lte_exists=phone['lte_exists'],
+                       slug=slugify(phone['name']))
+            ph.save()
+            print(ph)
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    name = models.CharField(max_length=250 nul=False)
+    name = models.CharField(max_length=250, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField
-    release_date = models.DateField
-    lte_exist = models.BooleanField
-    slug = models.SlugField(max_length=200) #Если не указывать входной параметр db_column для поля SlugField, то автоматически будет использовать значение из поля name
+    image = models.CharField(max_length=300, null=False)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=200) # noqa Если не указывать входной параметр db_column для поля SlugField, то автоматически будет использовать значение из поля name
+
+    def __str__(self):
+        return self.name
