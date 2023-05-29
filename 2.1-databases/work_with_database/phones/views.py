82b33daf128762f80cbf5e7df1531226d9665d96
Programmistochka <programmistochka@list.ru>
2023-05-29 15:32:47 +1200

    Task_3 views catalog and product ok

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,14 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones_inf = Phone.objects.all()
+    context = {'phones': phones_inf,
+               'sort': sort_type}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
