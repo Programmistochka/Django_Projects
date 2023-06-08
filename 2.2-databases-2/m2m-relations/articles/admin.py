5f86f6ee0c59772e06a456741437e0135f941f67
Programmistochka <programmistochka@list.ru>
2023-06-09 10:37:07 +1200

    Task_2.2.2 m2m-relations add models and adm panel

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Tag, Scope
 
+class ScopesInline(admin.TabularInline):
+    model = Scope
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [ScopesInline]
