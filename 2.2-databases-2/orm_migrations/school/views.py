3610f0db021ddbfdfe3f3c759f2ba481dd11a52e
Programmistochka <programmistochka@list.ru>
2023-06-19 17:59:07 +1200

    Task 2.2.1 orm_migrations add prefetch_related

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -10,5 +10,5 @@ def students_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-    context = {'object_list': students.order_by(ordering)}
+    context = {'object_list': students.prefetch_related("teachers").order_by(ordering)}
     return render(request, template, context)
