225cba8ccceea2753ed5c0003d46819e808d32b8
Programmistochka <programmistochka@list.ru>
2023-06-27 16:22:08 +1200

    Task 3.1 smart_home add serializers and views for sensors

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,16 @@
 from rest_framework import serializers
+from .models import Sensor, Measurement
 
 # TODO: опишите необходимые сериализаторы
+
+class SensorSerializer (serializers.Serializer):
+    
+    class Meta:
+        model = Sensor
+        fields = ['name', 'description']
+
+class MeasurementSerializer (serializers.Serializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['id', 'temperature', 'created_at']
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,31 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from .models import Measurement, Sensor
+from .serializers import MeasurementSerializer, SensorSerializer
+
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+# Спец. класс для управления данными по датчикам с помощью HTTP-запросов
+class SensorsView (ListAPIView):
+    # Вывод всех датчиков по GET-запросу:
+    # GET {{baseUrl}}/sensors/
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    # Создание нового датчика по POST-запросу:
+    # POST {{baseUrl}}/sensors/
+    # Указываются название и описание датчика
+    def post (self, request):
+        return Response({'status': 'ok'})
+
+    # Изменение датчика
+    # Указываются название и описание
+    def patch (self, request):
+        return Response({'status': 'ok'})
+
+class SensorView (RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
